66295f2a65769e317620b57a89684755e4275925
Darren Bowers <bic35@txstate.edu>
2025-09-04 22:25:37 -0500

    Created simple hello world main.cpp and main.h

--- main.cpp
+++ main.cpp
@@ -1,1 +1,7 @@
-/*** Test ***/
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
